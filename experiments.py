d481a930e2cbc5a0b2f8b31fc455ced84ec3ede1
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-04-01 13:29:00 +0200

    add scheduling config

--- experiments.py
+++ experiments.py
@@ -21,6 +21,8 @@ class SolverRunner(Runner):
         model_kwargs = config['model_kwargs']
         model = ModelClass(tasks, calendars, same_allocation, **model_kwargs)
 
+        solver_kwargs = config['solver_kwargs']
+
         objective = config['objective']
         if objective == "nb_teams":
             objective = model.get_nb_teams_objective()
@@ -31,23 +33,37 @@ class SolverRunner(Runner):
         
         model.minimize(objective)
 
+        timings = dict()
+        if isinstance(model, SchedulingModel) and config['compute_lb'] is True:
+            t0 = time.time()
+            lb = model.get_lower_bound(add_to_model=True, 
+                                       num_workers=solver_kwargs['num_workers'], 
+                                       time_limit=60)
+            t1 = time.time()
+            timings["lb_computation_time"] = t1 - t0
+            timings["lb_value"] = lb
+
         return {
             "model": model,
-            **config['solver_kwargs']
+            "solver_kwargs": solver_kwargs,
+            **timings
         }
+    def description(self, config):
+        return f"Solving {config['instance'].split('/')[-1]} with " + ",".join(f"{k}={v}" for k, v in config['model_kwargs'].items()) + f" using {config['solver_kwargs']['solver']}"
 
 
-def solve_model(model, solver, **solver_kwargs):
+def solve_model(model, solver_kwargs, **timings):
 
     t0 = time.time()
-    res = model.solve(solver=solver, **solver_kwargs)
+    res = model.solve(**solver_kwargs)
     t1 = time.time()
     return {
         "wallclock_time": t1 - t0,
         "solver_time": model.status().runtime,
         "objective_value": model.objective_value(),
         "status": str(model.status().exitstatus),
-        "satisfiable": res
+        "satisfiable": res,
+        **timings
     }
 
 if __name__ == "__main__":
